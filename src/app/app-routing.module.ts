702af76445c78cda10f302ebd24e52f69ffaec29
Shashank <shashank@crowdanalytix.net>
2022-08-01 18:00:26 +0530

    update for Profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './_components/home/home.component';
+import { ProfileComponent } from './_components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component : HomeComponent
+  },
+  {
+    path:'',
+    component : ProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './_components/_shared/top-bar/top-bar.component';
+import { ProfileComponent } from './_components/profile/profile.component';
+import { HomeComponent } from './_components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
