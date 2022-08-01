fe195428ac989a23b84b92ad1cdbbc040148ddce
Shashank <shashank@crowdanalytix.net>
2022-08-01 16:51:54 +0530

    Update for Main

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static(__dirname + '/dist/heroku-deploy'));
+app.use(express.static(__dirname + '/dist/profile'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/heroku-deploy/index.html'));
+res.sendFile(path.join(__dirname+'/dist/profile/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
